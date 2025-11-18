2de3c351b8143485ce04b52e139e335837f17089
hyeeuncho <heeuncho@naver.com>
2025-11-18 14:34:48 +0900

    프로젝트 세팅

---
+++ src/test/java/com/rdc/weflow_server/WeflowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdc.weflow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeflowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
