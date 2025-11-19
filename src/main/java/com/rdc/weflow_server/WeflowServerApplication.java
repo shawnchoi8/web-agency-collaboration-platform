5dee570e9b7ee73d32db3088c40613a01e39b190
hyeeuncho <heeuncho@naver.com>
2025-11-19 17:27:02 +0900

    test: jenkins test2

--- src/main/java/com/rdc/weflow_server/WeflowServerApplication.java
+++ src/main/java/com/rdc/weflow_server/WeflowServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeflowServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeflowServerApplication.class, args);
 	}
