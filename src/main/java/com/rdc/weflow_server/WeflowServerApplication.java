90e5152e47783893caa522c66445734e7befd3cb
hyeeuncho <heeuncho@naver.com>
2025-11-19 18:20:43 +0900

    test: jenkins test - docker제거

--- src/main/java/com/rdc/weflow_server/WeflowServerApplication.java
+++ src/main/java/com/rdc/weflow_server/WeflowServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeflowServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WeflowServerApplication.class, args);
 	}
