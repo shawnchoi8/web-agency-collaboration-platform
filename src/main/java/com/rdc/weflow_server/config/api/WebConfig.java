d1504dce241df5b9b5e7bb89376b14badd17e965
hyeeuncho <heeuncho@naver.com>
2025-12-04 15:14:26 +0900

    feat: 프론트 CORS 설정

---
+++ src/main/java/com/rdc/weflow_server/config/api/WebConfig.java
@@ -0,0 +1,26 @@
+package com.rdc.weflow_server.config.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000"   // React local
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
