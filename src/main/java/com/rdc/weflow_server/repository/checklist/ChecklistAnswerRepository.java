61c5a7d0e30737330908ffef5713d58dd879b40e
hyeeuncho <heeuncho@naver.com>
2025-12-04 18:57:37 +0900

    refactor: 체크리스드 중복 응답 방지

--- src/main/java/com/rdc/weflow_server/repository/checklist/ChecklistAnswerRepository.java
+++ src/main/java/com/rdc/weflow_server/repository/checklist/ChecklistAnswerRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChecklistAnswerRepository extends JpaRepository<ChecklistAnswer, Long> {
     List<ChecklistAnswer> findByChecklist_Id(Long checklistId); // 체크리스트별 답변 조회
+    Optional<ChecklistAnswer> findByChecklist_IdAndQuestion_Id(Long checklistId, Long questionId); // 특정 체크리스트 + 특정 질문 → 기존 답변 1개 조회 (업데이트용)
+
 }
