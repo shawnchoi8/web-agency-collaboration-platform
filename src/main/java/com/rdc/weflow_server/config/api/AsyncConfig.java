1e7655167fa41d4a662220d220a0f010db876d2f
PioKwon <4307yh@naver.com>
2025-12-16 15:11:38 +0900

    WIP: 작업 중간 저장

---
+++ src/main/java/com/rdc/weflow_server/config/api/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.rdc.weflow_server.config.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
